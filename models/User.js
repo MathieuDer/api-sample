5a972825718a3376dc47b65c03aa50c0303e0fbe
Mathieu Derouiche <mathieu.derouiche@ynov.com>
2020-01-20 10:52:42 +0100

    Add JWT

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongooose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongooose.Schema({
     name: {
@@ -40,5 +41,11 @@ UserSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
 });
 
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+}
 
 module.exports = mongooose.model('User', UserSchema);
