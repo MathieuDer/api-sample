e2e2fe8755cb3dc2a5a1ba68d159e2f52918b3a1
Mathieu Derouiche <mathieu.derouiche@ynov.com>
2020-02-03 14:55:03 +0100

    Add auth middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,6 +62,10 @@ const sendTokenResponse = (user, statusCode, res) => {
         httpOnly: true,
     };
 
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
     res
         .status(statusCode)
         .cookie('token', token, options)
@@ -70,3 +74,17 @@ const sendTokenResponse = (user, statusCode, res) => {
             token
         })
 }
+
+// @desc    Get current login  user
+// @route   POST /api/v1/auth/me
+// @access  Private
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
