74724c0e1cd95c7d4ec431d39072f0f6055a6f07
Mathieu Derouiche <mathieu.derouiche@ynov.com>
2020-02-11 17:20:35 +0100

    Get single review, review model, get reviews

---
+++ routes/reviews.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const {
+    getReviews,
+    getReview
+} = require('../controllers/reviews')
+
+const Review = require('../models/Review');
+const advancedResults = require('../middleware/advancedResults');
+
+const router = express.Router({ mergeParams: true });
+
+const { protect, authorize } = require('../middleware/auth');
+
+router.route('/')
+    .get(advancedResults(Review, {
+        path: 'bootcamp',
+        select: 'name description'
+    }),
+        getReviews
+    )
+
+router.route('/:id').get(getReview);
+
+module.exports = router;
